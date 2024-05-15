34d6a58a4806b34016418034a06120a0d17e30c8
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2024-05-16 01:50:58 +0300

    updating the types

--- types.ts
+++ types.ts
@@ -1,16 +1,19 @@
 export type Category = {
+    icon: any;
     id:number;
     name:string;
     color:string;
 }
 
 export type Slider = {
+    attributes: any;
     id:number;
     name:string;
     url:string;
 }
 
 export type MostCategory = {
+    attributes: any;
     id:number;
     url:string;
 }
