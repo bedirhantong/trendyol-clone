619d04ee79e4035e644014e235234354bd1fab21
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2024-05-16 01:33:02 +0300

    error resolved : Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities

--- components/Navbar/NavbarTop.tsx
+++ components/Navbar/NavbarTop.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const NavbarTop = () => {
   return (
     <div className="px-40 py-2 flex justify-end font-extralight-bold text-gray-400 text-xs gap-4">
-        <a href="*" className="hover:underline">İndirim Kuponlarım</a>
-        <a href="*" className="hover:underline">Trendyol'da Satış Yap</a>
-        <a href="*" className="hover:underline">Hakkımızda</a>
-        <a href="*" className="hover:underline">Yardım&Destek</a>
+        <a href="*" className="hover:underline">İndirim Kuponlarim</a>
+        <a href="*" className="hover:underline">Trendyolda Satis Yap</a>
+        <a href="*" className="hover:underline">Hakkimizda</a>
+        <a href="*" className="hover:underline">Yardim&Destek</a>
     </div>
   )
 }
