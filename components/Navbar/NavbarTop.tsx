38e204754515b0c5678eff57c442be7bdfed9fdd
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2024-05-16 01:31:06 +0300

    uneccessary usage is removed for deployment

--- components/Navbar/NavbarTop.tsx
+++ components/Navbar/NavbarTop.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const NavbarTop = () => {
   return (
-    <div className='px-40 py-2 flex justify-end font-extralight-bold text-gray-400 text-xs gap-4'>
-        <a href="*" className='hover:underline'>İndirim Kuponlarım</a>
-        <a href="*" className='hover:underline'>Trendyol'da Satış Yap</a>
-        <a href="*" className='hover:underline'>Hakkımızda</a>
-        <a href="*" className='hover:underline'>Yardım&Destek</a>
+    <div className="px-40 py-2 flex justify-end font-extralight-bold text-gray-400 text-xs gap-4">
+        <a href="*" className="hover:underline">İndirim Kuponlarım</a>
+        <a href="*" className="hover:underline">Trendyol'da Satış Yap</a>
+        <a href="*" className="hover:underline">Hakkımızda</a>
+        <a href="*" className="hover:underline">Yardım&Destek</a>
     </div>
   )
 }
